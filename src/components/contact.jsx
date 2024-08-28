12cb50ce60376dff8add1b4d95378329dded2a37
JTarsa <jan.tarsa@protonmail.com>
2024-08-28 12:06:56 -0700

    credentials

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -24,7 +24,7 @@ export const Contact = (props) => {
     {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
     
     emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
+      .sendForm("service_ppifobf", "template_822mftg", e.target, "IKwTcbRz4Vop-b0l5")
       .then(
         (result) => {
           console.log(result.text);

518264c32860ef0ff1f1703d0c119a3f16258e84
JTarsa <jan.tarsa@protonmail.com>
2024-08-28 12:09:58 -0700

    email

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -24,7 +24,7 @@ export const Contact = (props) => {
     {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
     
     emailjs
-      .sendForm("service_ppifobf", "template_822mftg", e.target, "IKwTcbRz4Vop-b0l5")
+      .sendForm("service_ppifobf", "template_uolcj9u", e.target, "IKwTcbRz4Vop-b0l5")
       .then(
         (result) => {
           console.log(result.text);
